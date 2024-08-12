6d0ffa13d0375cca77e07b0994960607cc90c15a
Coconut8201 <cocom714kv@gmail.com>
2024-08-12 23:02:57 +0800

    aaaa2

--- src/controller/voiceController.ts
+++ src/controller/voiceController.ts
@@ -4,7 +4,6 @@ import { DataBase } from "../utils/DataBase";
 import express from 'express';
 import { spawn } from 'child_process'; // child_process
 
-///aa
 export class VoiceController extends Controller{
     public test(Request:Request, Response:Response){
         Response.send(`This is VoiceController`);
